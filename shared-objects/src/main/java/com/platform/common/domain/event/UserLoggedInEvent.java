d4b149718468b6f305abcd01d06666180f8be4a3
Sarfraz Mughal <sarfraz.mughal1979@gmail.com>
2026-04-30 16:18:06 -0700

    Error removal

--- shared-objects/src/main/java/com/platform/common/domain/event/UserLoggedInEvent.java
+++ shared-objects/src/main/java/com/platform/common/domain/event/UserLoggedInEvent.java
@@ -2,13 +2,18 @@ package com.platform.common.domain.event;
 
 import com.platform.core.event.DomainEvent;
 import lombok.Getter;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class UserLoggedInEvent extends DomainEvent {
     private final String userId;
     private final String sessionId;
 
-    public UserLoggedInEvent(String userId, String sessionId) {
+	@JsonCreator
+    public UserLoggedInEvent(@JsonProperty("userId") String userId, @JsonProperty("sessionId") String sessionId) {
         super();
         this.userId = userId;
         this.sessionId = sessionId;
